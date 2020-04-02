916d143457dfb7c0524c26671249319f3141485d
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-04-02 11:31:51 +0800

    Added test_util_render.

--- tests/testthat/test_util_render.R
+++ tests/testthat/test_util_render.R
@@ -19,6 +19,23 @@ wd <- system.file(
   package = "jeksterslabRutils"
 )
 util_render(
+  recursive = TRUE,
   dir = wd,
   par = FALSE
 )
+util_render(
+  recursive = FALSE,
+  files = file.path(wd, "z.Rmd"),
+  par = FALSE
+)
+#'
+#+ testthat_01, echo=TRUE
+test_that("expect_error", {
+  expect_error(
+    util_render(
+      recursive = FALSE,
+      files = character(0),
+      par = FALSE
+    )
+  )
+})
