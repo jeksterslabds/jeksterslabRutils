491fae579a6bfe4076e4a60a707a7cac6f922d89
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-04-02 23:10:33 +0800

    Moved from jeksterslabRlib.

---
+++ R/util_shiny.R
@@ -0,0 +1,41 @@
+#' View Data Using a Shiny App
+#'
+#' inst/shiny/app
+#' 
+#' @author Ivan Jacob Agaloos Pesigan
+#' @param app Character string.
+#'   Name of the Shiny App.
+#' @importFrom shiny runApp
+#' @export
+util_shiny <- function(app, package) {
+  valid <- list.files(
+    system.file(
+      "shiny",
+      package = package
+    )
+  )
+  msg <- paste0(
+    "Valid apps are: '",
+    paste(
+      valid,
+      collapse = "', '"
+    ),
+    "'"
+  )
+  if (missing(app) || !nzchar(app) || !app %in% valid) {
+    stop(
+      "Please run `util_shiny()` with a valid app as an argument.\n",
+      msg,
+      call. = FALSE
+    )
+  }
+  dir <- system.file(
+    "shiny",
+    app,
+    package = package
+  )
+  runApp(
+    dir,
+    display.mode = "normal"
+  )
+}
