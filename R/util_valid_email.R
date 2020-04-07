629ba69716d474879be4e0021703690778ccc43f
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-04-08 02:27:39 +0800

    Added utility to validate email.

---
+++ R/util_valid_email.R
@@ -0,0 +1,13 @@
+#' Validate Email Address
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#' @param x Character string.
+#'   Email address to validate.
+#' @export
+util_valid_email <- function(x) {
+	grepl(
+        pattern = "\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>",
+        x = as.character(x),
+        ignore.case = TRUE
+    )
+}
